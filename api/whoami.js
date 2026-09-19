1402a1da2b7b25b695b4da373873d6b1ce950f0d
nifelux <globalpaysure@gmail.com>
2026-09-19 18:30:16 +0100

    Delete api/whoami.js

--- api/whoami.js
+++
@@ -1,5 +0,0 @@
-module.exports = async function handler(req, res) {
-  const r = await fetch('https://api.ipify.org?format=json');
-  const d = await r.json();
-  res.status(200).json({ egressIp: d.ip });
-};
